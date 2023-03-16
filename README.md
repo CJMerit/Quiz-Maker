f40ce8825f216b74b27aac4f500bb7983d0272a8
Connor McGrath <connorjmcgrath93@gmail.com>
2023-03-16 11:55:21 -0700

    readme and minor script changes

---
+++ README.md
@@ -0,0 +1,18 @@
+# My Portfolio
+
+## Description
+
+Do you want to test your web development knowledge? You can take this quiz
+and see just how much you've learned! compare your high scores to see how
+you're doing.
+
+![Webpage snippet](./assets/quiz-callenge.png)
+
+## Installation
+
+No install necessary!
+
+## Usage
+
+Just click start, and the quiz will begin! You'll get scored, and you can save your high scores.
+Correct answers will add a few seconds, while incorrect answers will take them away!
--- assets/script.js
+++ assets/script.js
@@ -15,6 +15,26 @@ const questions = [
     options: ['git push', 'git clone', 'git repo', 'git pull'], correct: 2},
     {question: 'Which HTML element creates a bullet point list?',
     options: ['ol', 'h1', 'ul', 'p'], correct: 3},
+    {question: 'This attribute is unique and can only be used once in the HTML file.',
+    options: ['id', 'class', 'href', 'src'], correct: 1},
+    {question: 'Which of these can be used as selectors in CSS?',
+    options: ['class', 'id', 'element', 'all of these'], correct: 4},
+    {question: 'This sets the boldness of the font in CSS.',
+    options: ['font-size', 'font-weight', 'line-height', 'font-family'], correct: 2},
+    {question: 'In CSS, this sets the font color of the selected HTML item.',
+    options: ['color', 'font-family', 'background-color', 'height'], correct: 1},
+    {question: 'In CSS, use this to NOT display the selected HTML item.',
+    options: ['display: inline', 'display: block', 'display: none', 'display'], correct: 3},
+    {question: 'In CSS, use this to NOT display the selected HTML item.',
+    options: ['display: inline', 'display: block', 'display: none', 'display'], correct: 3},
+    {question: 'In CSS, which of these sets the spacing INSIDE the selected HTML element?',
+    options: ['margin', 'padding', 'border', 'line-height'], correct: 2},
+    {question: 'In CSS, which of these sets the position relative to the viewport?',
+    options: ['position: fixed', 'position: relative', 'position: absolute', 'position: static'], correct: 1},
+    {question: 'Which of these is used to make responsive designs in CSS?',
+    options: ['@responsive', 'position: relative', 'width', '@media'], correct: 4},
+    {question: 'Which of these turns the selected item into a flexbox?',
+    options: ['display', 'flex', 'display: flex', 'flexbox'], correct: 3},
 ]
 
 function loadInitial() {
@@ -26,6 +46,10 @@ function loadInitial() {
     cardText.textContent = 'Answer some questions about web development. See if you can get a high score! \r\n'
     cardText.textContent += 'Correct answers will add time, but incorrect answers will subtract time!'
     card.appendChild(startButton)
+}
+
+function highScore() {
+    
 }
 
 function wrongAnswer() {
@@ -46,7 +70,7 @@ function wrongAnswer() {
 function correctAnswer() {
     totalScore++;
     score.textContent = 'Score: ' + totalScore;
-    secondsLeft = secondsLeft + 5;
+    secondsLeft = secondsLeft + 2;
     cardText.textContent = 'Correct!'
     for ( let i = 0; i < 4; i++) {
         card.removeChild(answers[i])
@@ -75,7 +99,7 @@ function setTime() {
 
 function setQuestion() {
     
-    let randQuestion = Math.floor(Math.random() * 3);
+    let randQuestion = Math.floor(Math.random() * questions.length);
     cardTitle.textContent = questions[randQuestion].question;
     
 
