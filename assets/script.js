7ad5d14cd9f0992dae2c8b5165a632b1972afdf6
Connor McGrath <connorjmcgrath93@gmail.com>
2023-03-17 10:31:33 -0700

    Final product

--- assets/script.js
+++ assets/script.js
@@ -3,11 +3,14 @@ let timerEl = document.querySelector('#timer');
 let cardTitle = document.querySelector('.card-title');
 let cardText = document.querySelector('.card-text');
 let card = document.querySelector('#question-card');
+let board = document.querySelector('#questions')
 let score = document.querySelector('#score');
+let viewHS = document.querySelector('#high-scores');
+let highScores = [];
 let answers = [];
-let secondsLeft = 120;
+let secondsLeft = 20;
 let totalScore = 0;
-var timerInterval;
+let timerInterval;
 const questions = [
     {question: 'What terminal command makes a new directory?',
     options: ['cd', 'ls', 'touch', 'mkdir'], correct: 4},
@@ -38,6 +41,13 @@ const questions = [
 ]
 
 function loadInitial() {
+    highScores = JSON.parse(localStorage.getItem("highScores"));
+    if(viewHS.classList[2] === 'd-none') {
+        viewHS.classList.remove('d-none')
+    }
+    if(!card.classList[4]) {
+        card.classList.add('h-75')
+    }
     totalScore = 0;
     score.textContent = '';
     timerEl.textContent = '';
@@ -49,11 +59,39 @@ function loadInitial() {
 }
 
 function highScore() {
-    
+    cardTitle.textContent = 'Finished!'
+    cardText.textContent = 'Enter name:'
+    let nameEntry = document.createElement("input")
+    nameEntry.setAttribute('type', 'text')
+    nameEntry.classList.add('text-group', 'mr-3')
+    card.appendChild(nameEntry)
+    let submitEntry = document.createElement('button')
+    submitEntry.textContent = 'Submit'
+    submitEntry.setAttribute('type', 'button')
+    submitEntry.classList.add("btn", "btn-success", "w-auto")
+    card.appendChild(submitEntry)
+
+    submitEntry.addEventListener('click', function() {
+        let name = nameEntry.value;
+        let setScore = score.textContent.split(' ')[1];
+
+        if(name && setScore) {
+            let newHighScore = {
+                name: name,
+                score: setScore
+            }
+            highScores.push(newHighScore)
+            localStorage.setItem('highScores', JSON.stringify(highScores))
+        }
+        card.removeChild(nameEntry)
+        card.removeChild(submitEntry)
+        loadInitial();
+    })
+
 }
 
 function wrongAnswer() {
-    secondsLeft = secondsLeft - 10;
+    secondsLeft = secondsLeft - 5;
     cardText.textContent = 'Incorrect!'
     for ( let i = 0; i < 4; i++) {
         card.removeChild(answers[i])
@@ -63,14 +101,14 @@ function wrongAnswer() {
     }
     else {
         clearInterval(timerInterval);
-        loadInitial();
+        highScore();
     }
 }
 
 function correctAnswer() {
     totalScore++;
     score.textContent = 'Score: ' + totalScore;
-    secondsLeft = secondsLeft + 2;
+    secondsLeft = secondsLeft + 1;
     cardText.textContent = 'Correct!'
     for ( let i = 0; i < 4; i++) {
         card.removeChild(answers[i])
@@ -79,19 +117,20 @@ function correctAnswer() {
 }
 
 function setTime() {
-    // Sets interval in variable    
+    // Sets interval in letiable    
     timerInterval = setInterval(function() {
         secondsLeft--;
-        timerEl.textContent = secondsLeft;
+        timerEl.textContent = 'Time: ' + secondsLeft;
 
-        if(secondsLeft === 0) {
+        if(secondsLeft <= 0 ) {
         // Stops execution of action at set interval
             clearInterval(timerInterval);
         // Calls function to create and append image
             for ( let i = 0; i < 4; i++) {
                 card.removeChild(answers[i])
             }
-            loadInitial();
+            secondsLeft = 20;
+            highScore();
         }
   
     }, 1000);
@@ -117,12 +156,56 @@ function setQuestion() {
     }
 }
 
+function viewHighScores() {
+    highScores = JSON.parse(localStorage.getItem("highScores"));
+    card.classList.remove('h-75')
+    let goBack = document.createElement('button')
+    goBack.textContent = 'Back'
+    goBack.setAttribute('type', 'button')
+    goBack.classList.add("btn", "btn-success", "w-auto", 'my-2')
+    card.appendChild(goBack)
+    card.removeChild(startButton)
+    card.removeChild(cardText)
+    cardTitle.textContent = 'High Scores!'
+    for(let i = 0; i < highScores.length; i++) {
+        let highScoreCard = document.createElement('div')
+        highScoreCard.classList.add('card', 'light', 'd-block', 'align-items-center', 'row')
+        let cardTitleHS = document.createElement('h3')
+        cardTitleHS.classList.add('card-title', 'my-2')
+        cardTitleHS.textContent = highScores[i].name;
+        let cardTextHS = document.createElement('p');
+        cardTextHS.classList.add('card-text', 'mb-2');
+        cardTextHS.textContent = highScores[i].score
+        highScoreCard.appendChild(cardTitleHS);
+        highScoreCard.appendChild(cardTextHS);
+        board.appendChild(highScoreCard);
+    }
+    goBack.addEventListener('click', function() {
+        event.stopPropagation()
+
+        for(let i = 0; i < highScores.length; i++) {
+            board.removeChild(board.children[1])
+        }
+        card.removeChild(goBack)
+        card.appendChild(cardText)
+        loadInitial();
+    })
+}
+
 startButton.addEventListener('click', function() {
     //Start Quiz function
+    timerEl.textContent = 'Time: ' + secondsLeft;
     score.textContent = 'Score: ' + totalScore;
     cardText.textContent = '';
     card.removeChild(startButton);
+    viewHS.classList.add('d-none')
     setTime();
     setQuestion();
 
+})
+
+viewHS.addEventListener('click', function() {
+    event.stopPropagation();
+    viewHS.classList.add('d-none');
+    viewHighScores();
 })
